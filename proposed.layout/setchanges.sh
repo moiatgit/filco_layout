0b5c235444546bc7f13ee9d30834caaa1032be55
Moisès Gómez Girón <moisesgomezgiron@gmail.com>
2015-06-08 13:13:03 +0200

    added es.catfilco

--- proposed.layout/setchanges.sh
+++ proposed.layout/setchanges.sh
@@ -1,6 +1,6 @@
 # cp -v ad /usr/share/X11/xkb/symbols
 # cp -v es /usr/share/X11/xkb/symbols
-cp -v jp /usr/share/X11/xkb/symbols
+cp -v jp es /usr/share/X11/xkb/symbols
 cp -v evdev.lst /usr/share/X11/xkb/rules
 cp -v base.lst /usr/share/X11/xkb/rules
 cp -v base.xml /usr/share/X11/xkb/rules
